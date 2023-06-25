cb9b67c403091e802b66c1c708d5bf751835183f
Kanish Mahato <72163903+corewithharry@users.noreply.github.com>
2023-06-25 21:56:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const _ = require("lodash");
+const debug = require('debug')('app');
 
 const app = express();
 
@@ -12,9 +11,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-
-// // mongoose.connect("mongodb+srv://Tron:jTzHQOHJQ0UalnSR@cluster0.wh2ictk.mongodb.net/?retryWrites=true&w=majority/todolistDB");
- mongoose.connect("mongodb+srv://Tron:jTzHQOHJQ0UalnSR@cluster0.wh2ictk.mongodb.net/todolistDB?retryWrites=true&w=majority");
+mongoose.connect("mongodb+srv://Tron:jTzHQOHJQ0UalnSR@cluster0.wh2ictk.mongodb.net/todolistDB?retryWrites=true&w=majority");
 
 const itemsSchema = {
   name: String
@@ -44,18 +41,18 @@ const List = mongoose.model("List", listSchema);
 app.get("/", function (req, res) {
   Item.find({})
     .then((items) => {
-      console.log(items);
+      debug(items);
       if (items.length == 0) {
         Item.insertMany(defaultItems)
           .then(() => {
-            console.log("Successfully saved items to the database");
+            debug("Successfully saved items to the database");
           })
           .catch((err) => {
             if (err && err.writeErrors && err.writeErrors.length > 0) {
-              console.log("Write errors occurred:");
-              console.log(err.writeErrors);
+              debug("Write errors occurred:");
+              debug(err.writeErrors);
             } else {
-              console.log(err);
+              debug(err);
             }
           });
         res.redirect("/");
@@ -64,7 +61,7 @@ app.get("/", function (req, res) {
       }
     })
     .catch((err) => {
-      console.log(err);
+      debug(err);
     });
 });
 
@@ -81,11 +78,11 @@ app.get("/:customlistname", async function(req, res) {
       await list.save();
       res.redirect("/" + customlistname); // Redirect to the newly created list
     } else {
-      console.log("List already exists!");
+      debug("List already exists!");
       res.render("list", { listTitle: foundList.name, newListItems: foundList.items });
     }
   } catch (err) {
-    console.error(err);
+    debug(err);
     res.status(500).send("Error occurred");
   }
 });
@@ -98,34 +95,34 @@ app.post("/", function (req, res) {
     name: itemName
   });
 
-  console.log("Item Name:", itemName);
-  console.log("List Name:", listname);
+  debug("Item Name:", itemName);
+  debug("List Name:", listname);
 
   if (listname === "Today") {
     item.save()
       .then(() => {
-        console.log("Item saved:", item);
+        debug("Item saved:", item);
         res.redirect("/");
       })
       .catch((err) => {
-        console.error("Error saving item:", err);
+        debug("Error saving item:", err);
       });
   } else {
     List.findOne({ name: listname })
       .then((foundList) => {
-        console.log("Found List:", foundList);
+        debug("Found List:", foundList);
         foundList.items.push(item);
         foundList.save()
           .then(() => {
-            console.log("Item added to list:", item);
+            debug("Item added to list:", item);
             res.redirect("/" + listname);
           })
           .catch((err) => {
-            console.error("Error saving list:", err);
+            debug("Error saving list:", err);
           });
       })
       .catch((err) => {
-        console.error("Error finding list:", err);
+        debug("Error finding list:", err);
       });
   }
 });
@@ -138,15 +135,15 @@ app.post("/delete", function(req, res) {
     Item.findByIdAndRemove(checkedItemId)
       .then(result => {
         if (result) {
-          console.log("Successfully deleted item:", result);
+          debug("Successfully deleted item:", result);
           res.redirect("/");
         } else {
-          console.log("Item not found.");
+          debug("Item not found.");
           res.status(404).send("Item not found.");
         }
       })
       .catch(err => {
-        console.error(err);
+        debug(err);
         res.status(500).send("Error deleting item");
       });
   } else {
@@ -158,7 +155,7 @@ app.post("/delete", function(req, res) {
         res.redirect("/" + listname);
       })
       .catch(err => {
-        console.error("Error updating list:", err);
+        debug("Error updating list:", err);
         res.status(500).send("Error updating list");
       });
   }
@@ -168,12 +165,6 @@ app.get("/about", function (req, res) {
   res.render("about");
 });
 
-// let port = process.env.PORT;
-// if (port == null || port == "") {
-//   port = 3000;
-// }
-// app.listen(port);
-
 app.listen(3000, function () {
-  console.log("Server started Succesfully");
+  debug("Server started successfully");
 });
